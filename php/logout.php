ea070b8b6ea4631c526a3e6991e6c55c6afe3d18
Ayush <ayush.beekum@gmail.com>
2025-05-25 01:16:20 +0200

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,11 +1,14 @@
 <?php
+session_start();
 
-setcookie('apiKey', '', time() - 36000, '/');
-setcookie('isAdmin', '', time() - 36000, '/');
-setcookie('productId', '', time() - 36000, '/');
+setcookie('api_key', '', time() - 3600, '/');
+setcookie('isAdmin', '', time() - 3600, '/');
+setcookie('productId', '', time() - 3600, '/');
 
+// destroy the session* NB!!!
+session_destroy();
 
+// go to home page
 header('Location: home.php');
-
-
+exit();
 ?>
