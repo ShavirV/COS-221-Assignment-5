aeb6cdb4f1eaad3bfb8158c3c0c6aab4a574016d
ShavirV <shavirvallabh05@gmail.com>
2025-05-26 15:25:58 +0200

    PHPPPPPPPP

--- php/admin.php
+++ php/admin.php
@@ -18,19 +18,7 @@ if (!isset($_COOKIE['api_key']) || !isset($_COOKIE['isAdmin']) || $_COOKIE['isAd
     exit;
 }
 ?>
-<!DOCTYPE html>
-< lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Admin - Products Management</title>
-    <link rel="stylesheet" href="../css/admin.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    />
-  </head>
-  <>
+
     <div class="background-overlay"></div>
 
     <nav class="top-navbar">
